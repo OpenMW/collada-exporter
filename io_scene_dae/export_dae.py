5e2bcb4a722e49794b58f49ce44f88eda42469ca
jitspoe <jitspoe@yahoo.com>
2020-04-07 00:03:33 -0400

    Fix for modifiers, such as the mirror modifier, not applying on export.
    (cherry picked from commit bf07d01db0ebb66d8df878d315a95d7240309468)

--- io_scene_dae/export_dae.py
+++ io_scene_dae/export_dae.py
@@ -204,7 +204,7 @@ class DaeExporter:
                     "images", os.path.basename(image.filepath))
                 image.filepath = img_tmp_path
 
-        
+
         else:
             try:
                 imgpath = os.path.relpath(
@@ -212,7 +212,7 @@ class DaeExporter:
             except:
                 # TODO: Review, not sure why it fails
                 pass
-        
+
         imgid = self.new_id("image")
 
         print("FOR: {}".format(imgpath))
@@ -234,19 +234,19 @@ class DaeExporter:
             fxid, material.name))
         self.writel(S_FX, 2, "<profile_COMMON>")
 
-        # Find and fetch the textures and create sources    
+        # Find and fetch the textures and create sources
         sampler_table = {}
         diffuse_tex = None
         specular_tex = None
         emission_tex = None
         normal_tex = None
-        
+
         #TODO, use Blender 2.8 principled shader and connected maps
         mat_wrap = node_shader_utils.PrincipledBSDFWrapper(material) if material else None
-        
+
         if mat_wrap:
             textures_keys = ["base_color_texture", "specular_texture", "normalmap_texture"]
-            
+
             for i, tkey in enumerate(textures_keys):
                 tex = getattr(mat_wrap, tkey, None)
                 if tex == None:
@@ -256,7 +256,7 @@ class DaeExporter:
 
                 # Image
                 imgid = self.export_image(tex.image)
-                
+
                 # Surface
                 surface_sid = self.new_id("fx_surf")
                 self.writel(S_FX, 3, "<newparam sid=\"{}\">".format(surface_sid))
@@ -265,7 +265,7 @@ class DaeExporter:
                 self.writel(S_FX, 5, "<format>A8R8G8B8</format>")
                 self.writel(S_FX, 4, "</surface>")
                 self.writel(S_FX, 3, "</newparam>")
-                
+
                 # Sampler
                 sampler_sid = self.new_id("fx_sampler")
                 self.writel(S_FX, 3, "<newparam sid=\"{}\">".format(sampler_sid))
@@ -274,7 +274,7 @@ class DaeExporter:
                 self.writel(S_FX, 4, "</sampler2D>")
                 self.writel(S_FX, 3, "</newparam>")
                 sampler_table[i] = sampler_sid
-                
+
                 if tkey == "base_color_texture" and diffuse_tex is None:
                     diffuse_tex = sampler_sid
                 if tkey == "specular_texture" and specular_tex is None:
@@ -286,7 +286,7 @@ class DaeExporter:
                 """
                 if tkey == "normalmap_texture" and normal_tex is None:
                     normal_tex = sampler_sid
-        
+
         """
         for i in range(len(material.texture_slots)):
             ts = material.texture_slots[i]
@@ -332,7 +332,7 @@ class DaeExporter:
             if ts.use_map_normal and normal_tex is None:
                 normal_tex = sampler_sid
         """
-        
+
         self.writel(S_FX, 3, "<technique sid=\"common\">")
         shtype = "blinn"
         self.writel(S_FX, 4, "<{}>".format(shtype))
@@ -343,9 +343,9 @@ class DaeExporter:
                 S_FX, 6, "<texture texture=\"{}\" texcoord=\"CHANNEL1\"/>"
                 .format(emission_tex))
         else:
-            # TODO: More accurate coloring, if possible     
+            # TODO: More accurate coloring, if possible
             self.writel(S_FX, 6, "<color>{}</color>".format(
-                numarr_alpha(material.diffuse_color, 1.0)))#material.emit is removed in Blender 2.8             
+                numarr_alpha(material.diffuse_color, 1.0)))#material.emit is removed in Blender 2.8
         self.writel(S_FX, 5, "</emission>")
 
         self.writel(S_FX, 5, "<ambient>")
@@ -391,7 +391,7 @@ class DaeExporter:
             self.writel(S_FX, 6, "<float>{}</float>".format(material.alpha))
             self.writel(S_FX, 5, "</transparency>")
         """
-        
+
         self.writel(S_FX, 5, "<index_of_refraction>")
         self.writel(S_FX, 6, "<float>{}</float>".format(1.2))#material.specular_ior is removed too
         self.writel(S_FX, 5, "</index_of_refraction>")
@@ -413,7 +413,7 @@ class DaeExporter:
         self.writel(S_FX, 6, "<double_sided>{}</double_sided>".format(
             int(double_sided_hint)))
         self.writel(S_FX, 5, "</technique>")
-    
+
         """
         if (material.use_shadeless):#material.use_shadeless is removed too
             self.writel(S_FX, 5, "<technique profile=\"GODOT\">")
@@ -439,7 +439,7 @@ class DaeExporter:
     def export_mesh(self, node, armature=None, skeyindex=-1, skel_source=None,
                     custom_name=None):
         mesh = node.data
-        
+
         if (node.data in self.mesh_cache):
             return self.mesh_cache[mesh]
 
@@ -462,35 +462,35 @@ class DaeExporter:
                 shape.value = 1.0
                 mesh.update()
                 p = node.data
-                
-                armature_modifier = None            
+
+                armature_modifier = None
                 armature_modifier_state = None
-                
+
                 if(self.config["use_exclude_armature_modifier"]):
                     armature_modifiers = [i for i in node.modifiers if i.type == "ARMATURE"]
                     armature_modifier = armature_modifiers[0]#node.modifiers.get("Armature")
 
-                if(armature_modifier):  
+                if(armature_modifier):
                     # the armature modifier must be disabled too
                     armature_modifier_state = armature_modifier.show_viewport
-                    armature_modifier.show_viewport = False         
-                
+                    armature_modifier.show_viewport = False
+
                 print(node)
-                v = node.to_mesh(preserve_all_data_layers=True, depsgraph=bpy.context.evaluated_depsgraph_get()) 
+                v = node.to_mesh(preserve_all_data_layers=True, depsgraph=bpy.context.evaluated_depsgraph_get())
                 print(v)
                 # Warning, Blender 2.8 does not support anymore the "RENDER" argument to apply modifier
                 # with render state only...
-                
+
                 armature_modifier.show_viewport = armature_modifier_state
-                
+
                 self.temp_meshes.add(v)
                 deps = bpy.context.evaluated_depsgraph_get()
                 evaluated_node = node.evaluated_get(deps)
                 evaluated_node.data = v
                 evaluated_node.data.update()
-                if (armature and k == 0):                   
+                if (armature and k == 0):
                     md = self.export_mesh(evaluated_node, armature, k, mid, shape.name)
-                else:                   
+                else:
                     md = self.export_mesh(evaluated_node, None, k, None, shape.name)
 
                 node.data = p
@@ -593,18 +593,18 @@ class DaeExporter:
         armature_modifier = None
         armature_poses = None
         armature_modifier_state = None
-        
+
         if(self.config["use_exclude_armature_modifier"]):
             armature_modifiers = [i for i in node.modifiers if i.type == "ARMATURE"]
             if len(armature_modifiers) > 0:
-                print(node.name)            
+                print(node.name)
                 armature_modifier = armature_modifiers[0]#node.modifiers.get("Armature")
 
         # Set armature in rest pose
-        if(armature_modifier):  
+        if(armature_modifier):
             # the armature modifier must be disabled too
             armature_modifier_state = armature_modifier.show_viewport
-            armature_modifier.show_viewport = False         
+            armature_modifier.show_viewport = False
             #doing this per object is inefficient, should be improved, maybe?
             armature_poses = [arm.pose_position for arm in bpy.data.armatures]
             for arm in bpy.data.armatures:
@@ -617,16 +617,19 @@ class DaeExporter:
         if (custom_name is not None and custom_name != ""):
             name_to_use = custom_name
 
-        mesh = node.to_mesh(preserve_all_data_layers=False, depsgraph=bpy.context.evaluated_depsgraph_get()) 
+        depgraph = bpy.context.evaluated_depsgraph_get()
+        if (apply_modifiers):
+            node = node.evaluated_get(depgraph)
+        mesh = node.to_mesh(preserve_all_data_layers=True, depsgraph=depgraph)
         # 2.8 update: warning, Blender does not support anymore the "RENDER" argument to apply modifier
         # with render state, only current state
-        
+
         # Restore armature and modifier state
-        if(armature_modifier):          
-            armature_modifier.show_viewport = armature_modifier_state           
+        if(armature_modifier):
+            armature_modifier.show_viewport = armature_modifier_state
             for i,arm in enumerate(bpy.data.armatures):
                 arm.pose_position = armature_poses[i]
-                
+
 
 
         self.temp_meshes.add(mesh)
@@ -685,8 +688,8 @@ class DaeExporter:
                     mat = mesh.materials[f.material_index]
                 except:
                     mat = None
-                
-                if (mat is not None):               
+
+                if (mat is not None):
                     materials[f.material_index] = self.export_material(
                         mat, True)#True = deprecated mesh.show_double_sided value, which is removed from Blender 2.8
                 else:
@@ -1138,8 +1141,8 @@ class DaeExporter:
                                         t.id.name in self.scene.objects):
                                     self.armature_for_morph[
                                         node] = self.scene.objects[t.id.name]
-        
-    
+
+
         meshdata = self.export_mesh(node, armature)
         close_controller = False
 
@@ -1540,7 +1543,7 @@ class DaeExporter:
             curveid))
         self.writel(S_NODES, il, "</instance_geometry>")
 
-    def export_node(self, node, il):        
+    def export_node(self, node, il):
         if (node not in self.valid_nodes):
             return
 
@@ -1592,7 +1595,7 @@ class DaeExporter:
                 if col.hide_viewport == True:
                     valid = False
                     break
-                    
+
             if (not valid):
                 return False
 
@@ -1618,7 +1621,7 @@ class DaeExporter:
                     n = n.parent
 
         for obj in sorted(self.scene.objects, key=lambda x: x.name):
-            if (obj in self.valid_nodes and obj.parent is None):                
+            if (obj in self.valid_nodes and obj.parent is None):
                 self.export_node(obj, 2)
 
         self.writel(S_NODES, 1, "</visual_scene>")
@@ -1897,13 +1900,13 @@ class DaeExporter:
             self.writel(S_ANIM_CLIPS, 0, "<library_animation_clips>")
 
             for x in bpy.data.actions[:]:
-                
+
                 if x.users == 0 or x in self.action_constraints:
                     continue
                 if (self.config["use_anim_skip_noexp"] and
                         x.name.endswith("-noexp")):
                     continue
-                           
+
                 bones = []
                 # Find bones used
                 for p in x.fcurves:
@@ -2059,7 +2062,7 @@ class DaeExporter:
 
     def __exit__(self, *exc):
         pass
-        """    
+        """
         for mesh in self.temp_meshes:
             bpy.data.meshes.remove(mesh)
         """
